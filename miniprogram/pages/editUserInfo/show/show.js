6292fb6367f6cd417089b767acc342892e601341
tianyi <2861218857@qq.com>
2020-04-02 23:23:58 +0800

    20/04/02/wodeOver

--- miniprogram/pages/editUserInfo/show/show.js
+++ miniprogram/pages/editUserInfo/show/show.js
@@ -1,11 +1,15 @@
 // miniprogram/pages/editUserInfo/show/show.js
+
+const app = getApp()
+const db = wx.cloud.database()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userPhoto : ''
   },
 
   /**
@@ -19,7 +23,9 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.setData({
+      userPhoto : app.userInfo.userPhoto
+    });
   },
 
   /**
@@ -62,5 +68,71 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  handleUploadImage(){
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],  //'original'   原图
+      sourceType: ['album', 'camera'],   //手机系统相册   拍照
+      success : (res)=>{
+        //console.log(res);
+        //tempFilePaths可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths[0];
+        this.setData({
+          userPhoto: tempFilePaths
+        });
+      }
+    })
+  },
+  handleBtn(){
+
+    wx.showLoading({
+      title: '上传中'
+    })
+    let cloudPath = "userPhoto/" + app.userInfo._openid + Date.now() + ".jpg";
+    wx.cloud.uploadFile({
+      cloudPath,
+      filePath: this.data.userPhoto  //临时路径
+    }).then((res)=>{
+      //console.log(res);
+      let fileID = res.fileID;
+      if(fileID){
+        db.collection('users').doc(app.userInfo._id).update({
+          data : {
+            userPhoto : fileID
+          }
+        }).then((res)=>{
+          wx.hideLoading();
+          wx.showToast({
+            title: '上传并更新成功'
+          });
+          app.userInfo.userPhoto = fileID;
+        });
+      }
+    });
+  },
+  bindGetUserInfo(ev){
+    let userInfo = ev.detail.userInfo;
+    if (userInfo) {
+      this.setData({
+        userPhoto: userInfo.avatarUrl
+      }, () => {
+        wx.showLoading({
+          title: '上传中'
+        })
+        db.collection('users').doc(app.userInfo._id).update({
+          data: {
+            userPhoto: userInfo.avatarUrl
+          }
+        }).then((res) => {
+          wx.hideLoading();
+          wx.showToast({
+            title: '上传并更新成功'
+          });
+          app.userInfo.userPhoto = userInfo.avatarUrl;
+        });
+        
+      });
+    }
   }
 })
